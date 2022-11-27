fb4d32ada469195ce9c1759b5956f11a6cdfea54
000ATSU <gaomuliren706@gmail.com>
2022-11-27 21:54:45 +0900

    [add]new file
    学習：繰り返し（for文）

---
+++ Javapro/src/main/java/projava/ForPra.java
@@ -0,0 +1,9 @@
+package projava;
+
+public class ForPra {
+  public static void main(String[] args) {
+    for (int i = 0; i < 36; i += 5) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ Javapro/src/main/java/projava/ForSample.java
@@ -0,0 +1,9 @@
+package projava;
+
+public class ForSample {
+  public static void main(String[] args) {
+    for (int n = 1; n < 11; n++) {
+      System.out.println(n);
+    }
+  }
+}
