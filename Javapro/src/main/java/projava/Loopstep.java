7cfb08129a5501935024f112566497a014102e6a
000ATSU <gaomuliren706@gmail.com>
2022-11-30 22:52:18 +0900

    [add]new file
    学習：繰り返し(二重ループ)

---
+++ Javapro/src/main/java/projava/Loopstep.java
@@ -0,0 +1,10 @@
+package projava;
+
+public class Loopstep {
+  public static void main(String[] args) {
+    for (int i = 5; i > 0; i--) {
+      System.out.print("0".repeat(i));
+      System.out.println();
+    }
+  }
+}
