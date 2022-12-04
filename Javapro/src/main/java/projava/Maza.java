d909b8cb02f4e8e846f16f14dc5360928f351e2f
000ATSU <gaomuliren706@gmail.com>
2022-12-04 22:00:39 +0900

    [edit]Maze.java
    編集：操作方法、ゴール位置変更、ゴール表示、無操作時ゲームリセット

--- Javapro/src/main/java/projava/Maza.java
+++ Javapro/src/main/java/projava/Maza.java
@@ -15,8 +15,8 @@ public class Maza {
             {1, 1, 1, 1, 1, 1}
     };
 
-    var current = new Position(1, 1);
-    var goal = new Position(4, 3);
+    var current = new Position(1, 3);
+    var goal = new Position(3, 1);
     for (; ; ) {
       for (int y = 0; y < map.length; y++) {
         for (int x = 0; x < map.length; x++) {
@@ -24,6 +24,8 @@ public class Maza {
             System.out.print("o");
           } else if (map[y][x] == 1) {
             System.out.print("*");
+          } else if (x == goal.x() && y == goal.y()) {
+            System.out.print("G");
           } else {
             System.out.print(".");
           }
@@ -36,14 +38,16 @@ public class Maza {
       }
       int ch = System.in.read();
       var next = switch (ch) {
-        case 'a' -> new Position(current.x() - 1, current.y());
-        case 'w' -> new Position(current.x(), current.y());
-        case 's' -> new Position(current.x() + 1, current.y());
-        case 'z' -> new Position(current.x(), current.y() + 1);
+        case 'u' -> new Position(current.x() - 1, current.y());
+        case 'h' -> new Position(current.x(), current.y() - 1);
+        case 'j' -> new Position(current.x() + 1, current.y());
+        case 'n' -> new Position(current.x(), current.y() + 1);
         default -> current;
       };
       if (map[next.y()][next.x()] == 0) {
         current = next;
+      } else if (next == current) {
+        System.exit(0);
       }
       System.in.read();
     }
