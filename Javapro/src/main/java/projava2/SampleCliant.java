a9db3c233a739be2d0715843ac6b5b26f7bf01d6
000ATSU <gaomuliren706@gmail.com>
2022-12-16 23:54:04 +0900

    [add]new file
    学習：通信

--- Javapro/src/main/java/projava2/SampleCliant.java
+++ Javapro/src/main/java/projava2/SampleCliant.java
@@ -2,14 +2,16 @@ package projava2;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class SampleCliant {
   public static void main(String[] args) throws IOException {
-    var soc = new Socket("localhost", 1700);
-    OutputStream output = soc.getOutputStream();
-    output.write(234);
-    output.close();
-    soc.close();
+    try (var soc = new Socket("localhost", 1700);
+         OutputStream output = soc.getOutputStream()) {
+      output.write(234);
+    } catch (ConnectException e) {
+      System.out.println("サーバーが起動していません");
+    }
   }
 }
