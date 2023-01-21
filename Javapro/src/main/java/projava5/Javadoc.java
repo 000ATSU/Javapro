49041b8b8d2f4dad5175caa3ef3cf93fcb354327
000ATSU <gaomuliren706@gmail.com>
2023-01-21 22:16:06 +0900

    [add]new file
    学習:Javadoc(再作成)

---
+++ Javapro/src/main/java/projava5/Javadoc.java
@@ -0,0 +1,23 @@
+package projava5;
+
+public class Javadoc {
+  /**
+   * 渡された西暦年が夏季近代オリンピック開催年であるかどうか判定する
+   *
+   * @param year 西暦
+   * @return 夏季オリンピック開催年であればture
+   * @throws IllegalArgumentException まだオリンピックを開催が確定していない年を渡した場合
+   */
+  public static boolean isSummerOlympicYear(int year) throws IllegalArgumentException {
+    if (2023 < year) {
+      throw new IllegalArgumentException("2023年までサポートしています。入力：" + year);
+    }
+    return year % 4 == 0;
+  }
+
+  public static void main(String[] args) {
+    boolean x = isSummerOlympicYear(2012);
+    System.out.println(x);
+  }
+
+}
